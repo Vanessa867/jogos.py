23a89535ed32eb3a405d8db008bc324ff22df3ed
Vanessa lins <69607861+Vanessa867@users.noreply.github.com>
2023-10-05 17:13:07 -0300

    Update pedrapapeletesoura.py
    erro concertado

--- pedrapapeletesoura.py
+++ pedrapapeletesoura.py
@@ -1,24 +1,25 @@
+import random
 def jogar_jokenpo():
     opcoes = ["pedra", "papel", "tesoura"]
 
-    jogador1 = input("Jogador1, escolha pedra, papel ou tesoura: ")
+    jogador1 = random.choice(opcoes)
 
     jogador2 = input("Jogador2, escolha pedra, papel ou tesoura: ")
 
-    if jogador1 and jogador2 not in opcoes:
+    if jogador1 not in opcoes:
         print("Escolha inválida. Por favor, escolha pedra, papel ou tesoura.")
         return
 
-    print(f"Você escolheu: {jogador1}")
-    print(f"O computador escolheu: {jogador2}")
+    print(f"Você escolheu: {jogador2}")
+    print(f"O computador escolheu: {jogador1}")
 
     if jogador1 == jogador2:
         print("Empate!")
     elif (
-        (jogador1 == "pedra" and jogador2 == "tesoura")
-        or (jogador1 == "papel" and jogador2 == "pedra")
-        or (jogador1 == "tesoura" and jogador2 == "papel")
-    ):
+        jogador1 == "pedra" and jogador2 == "tesoura"
+        or (jogador1 == "papel" and jogador2 == "pedra"
+        or jogador1 == "tesoura" and jogador2 == "papel"
+           ):
         print("Jogador1 ganhou!")
     else:
         print("Jogador2 ganhou!")
